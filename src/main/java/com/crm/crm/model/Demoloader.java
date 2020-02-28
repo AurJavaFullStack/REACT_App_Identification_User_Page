18f0eb0eaae8cfa310fabea1cc0b01e1138b584e
AurJavaFullStack <aur951@hotmail.fr>
2020-02-28 22:44:04 +0100

    first commit

---
+++ src/main/java/com/crm/crm/model/Demoloader.java
@@ -0,0 +1,43 @@
+package com.crm.crm.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component 
+public class Demoloader implements CommandLineRunner{
+
+    private final ContactRepository repository;
+
+    @Autowired
+    public Demoloader(ContactRepository repository){
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        this.repository.save(new Contact("Emmanuel","Henri","me@me.com"));
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/crm/crm/model/ReactAndSpringDataRestApplication.java
@@ -0,0 +1,15 @@
+package com.crm.crm.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAndSpringDataRestApplication{
+    public static void main(final String[] args) {
+        SpringApplication.run(ReactAndSpringDataRestApplication.class,args);
+    }
+}
+
+
+
+
